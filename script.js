5097f2d4b69ead33523755b045b3b92507bcafb1
nikoladobre <130917383+nikoladobre@users.noreply.github.com>
2024-03-20 21:20:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+    const slides = document.querySelectorAll('.slide');
+
+    function showSlide(n) {
+        if (n >= slides.length) { slideIndex = 0; }
+        if (n < 0) { slideIndex = slides.length - 1; }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.transition = "transform 0.5s ease";
+            slides[i].style.transform = `translateX(-${slideIndex * 100}%)`;
+        }
+    }
+
+    function nextSlide() {
+        showSlide(++slideIndex);
+    }
+
+    function prevSlide() {
+        showSlide(--slideIndex);
+    }
+
+    setInterval(nextSlide, 3000);
+
+    showSlide(slideIndex);
